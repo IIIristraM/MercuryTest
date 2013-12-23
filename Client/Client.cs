3c3bb27b261b681081781a1c5cf5762623160dc6
iiiristram <iiiristram@gmail.com>
2013-12-23 21:48:56 +0400

    home fix

---
+++ Client/Client.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class FileManagerClient: IDisposable
+    {
+        private FileManagerServiceProxy.FileManagerServiceClient _serviceProxy;
+        public FileManagerClient()
+        {
+            _serviceProxy = new FileManagerServiceProxy.FileManagerServiceClient(new InstanceContext(new Notificator()));
+            _serviceProxy.Open();
+        }
+
+        public string ExecuteCommand(string command)
+        {
+            command = command.ToLower();
+            Regex exp = new Regex(@"(\S+)(\s*)(\S*)");
+            var match = exp.Match(command);
+
+            string com = match.Groups[1].Captures[0].Value;
+            string param = match.Groups[3].Captures[0].Value;
+
+            string response;
+            switch (com)
+            {
+                case "connect":
+                    response = Connect(param);
+                    break;
+                case "quit":
+                    response = Quit();
+                    break;
+                case "md":
+                    response = CreateDirectory(param);
+                    break;
+                case "cd":
+                    response = ChangeDirectory(param);
+                    break;
+                default:
+                    response = "unknown command";
+                    break;
+            }
+            return response;
+        }
+
+        public string Connect(string userName)
+        {
+            return _serviceProxy.Connect(userName);
+        }
+
+        public string Quit()
+        {
+            return _serviceProxy.Quit();
+        }
+
+        public string CreateDirectory(string path)
+        {
+            return _serviceProxy.CreateDirectory(path);
+        }
+
+        public string ChangeDirectory(string path)
+        {
+            return _serviceProxy.ChangeDirectory(path);
+        }
+
+        public void Dispose()
+        {
+            _serviceProxy.Close();
+        }
+    }
+}
---
+++ UnitTests/FileManagerServiceTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileManagerService;
+using Domain;
+using Microsoft.QualityTools.Testing.Fakes;
+using System.ServiceModel;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class FileManagerServiceTest
+    {
+        IFileSystemService _fileSystemService;
+        IFileManagerService _fileManagerService;
+
+        public FileManagerServiceTest()
+        {
+            _fileSystemService = new FileSystemService();
+            _fileManagerService = new FileManagerService.FileManagerService(_fileSystemService);
+        }
+
+        [TestMethod]
+        public void Connect()
+        {
+            var userName = "Konstantin";
+
+            var result = _fileManagerService.Connect(userName);
+            Assert.AreEqual(result, @"c:>");
+
+            result = _fileManagerService.Connect(userName);
+            Assert.AreEqual(result, "Error: User has been connected already");
+        }
+
+        [TestMethod]
+        public void CreateDirectory()
+        {
+            var userName = "Konstantin";
+            var path = @"c:\temp";
+            _fileManagerService.Connect(userName);
+
+            var result = _fileManagerService.CreateDirectory(path);
+            Assert.AreEqual(result, @"Directory 'c:\temp' created");
+
+            path = "temp";
+
+            result = _fileManagerService.CreateDirectory(path);
+            Assert.AreEqual(result, @"Error: Directory 'c:\temp' already exists");
+
+            path = @"temp2\temp3";
+
+            result = _fileManagerService.CreateDirectory(path);
+            Assert.AreEqual(result, @"Error: Directory 'c:\temp2' doesn't exist");
+        }
+
+        [TestMethod]
+        public void ChangeDirectory()
+        {
+            var userName = "Konstantin";
+            var path = "temp";
+            _fileManagerService.Connect(userName);
+            var result = _fileManagerService.CreateDirectory(path);
+
+            result = _fileManagerService.ChangeDirectory(path);
+            Assert.AreEqual(result, @"c:\temp>");
+
+            path = @"temp2\temp3";
+            _fileManagerService.Connect(userName);
+
+            result = _fileManagerService.ChangeDirectory(path);
+            Assert.AreEqual(result, @"Error: Directory 'c:\temp\temp2\temp3' doesn't exist");
+        }
+    }
+}
